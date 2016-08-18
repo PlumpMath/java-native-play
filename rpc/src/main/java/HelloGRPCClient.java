327556f3fd9cc9dcad79a63f6960c3731db6044c
Jack Kelly <jack@fullcontact.com>
2016-08-18 08:36:47 -0600

    Add ability to detect faces in specified image

--- rpc/src/main/java/HelloGRPCClient.java
+++ rpc/src/main/java/HelloGRPCClient.java
@@ -4,22 +4,23 @@ import com.google.protobuf.ByteString;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.Random;
+import java.nio.file.Files;
 
 public class HelloGRPCClient {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File file = new File(args[0]);
+        byte[] photoBytes = Files.readAllBytes(file.toPath());
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
                 .usePlaintext(true)
                 .build();
-        Random random = new Random();
-        byte[] bytes = new byte[20];
-        random.nextBytes(bytes);
         PrinterGrpc.PrinterBlockingStub blockingStub = PrinterGrpc.newBlockingStub(channel);
         NativeService.PrintRequest message = NativeService.PrintRequest.newBuilder()
                 .setMsg("Message")
-                .setPayload(ByteString.copyFrom(bytes))
+                .setPayload(ByteString.copyFrom(photoBytes))
                 .build();
         NativeService.PrintReply printReply = blockingStub.printMsg(message);
         System.out.println(printReply.getMsg());
