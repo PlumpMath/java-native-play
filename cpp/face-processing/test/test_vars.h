5eaa4c747aeead27748a523e630cef1f015f6856
Jack Kelly <jack@fullcontact.com>
2016-12-06 11:59:49 -0700

    Add missing test variables file

---
+++ cpp/face-processing/test/test_vars.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// Template file which gets pre-processed by CMake to inject the directory for test data
+const char* DATA_PATH = "/Users/jack/java_native_play/cpp/face-processing/test/data";
+
+// If more paths to directories in the source tree are needed, add them below
